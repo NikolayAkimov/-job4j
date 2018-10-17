cd86f8f6f86088e7410f61a41c6daa7ccb131b8b
NikolayAkimov <mr.kolis@gmail.com>
2018-10-17 13:39:10 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Class Класс для вывода строки в консоль.
+* @author nakimov.
+* @since 17.10.2018.
+* @version 1.
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Nikolay Akimov";
+    String expect = "Echo, echo, echo : Nikolay Akimov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
