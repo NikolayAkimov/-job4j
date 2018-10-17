69f6ddb6d3ea7f4f2a7ed33df973b6de09087d2e
NikolayAkimov <mr.kolis@gmail.com>
2018-10-17 12:13:36 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j.calculate;
+/**
+* Class Класс для вывода строки в консоль.
+* @author nakimov.
+* @since 17.10.2018.
+* @version 1.
+*/
 public class Calculate{
+	/**
+	* Main.
+	* @param args - args.
 
  public static void main(String[] args){
    System.out.println("Hello World");
