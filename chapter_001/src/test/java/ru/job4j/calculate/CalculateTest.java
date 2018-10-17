c609cf089ec3385439aa7918f310eb768b25b8b9
NikolayAkimov <mr.kolis@gmail.com>
2018-10-17 16:21:12 +0300

    проверка

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,22 +5,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Class Класс для вывода строки в консоль.
-* @author nakimov.
-* @since 17.10.2018.
-* @version 1.
-*/
+ * Class Класс для вывода строки в консоль.
+ * @author nakimov.
+ * @since 17.10.2018.
+ * @version 1.
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Nikolay Akimov";
-    String expect = "Echo, echo, echo : Nikolay Akimov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Nikolay Akimov";
+        String expect = "Echo, echo, echo : Nikolay Akimov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
